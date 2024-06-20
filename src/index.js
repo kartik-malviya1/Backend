1f57dea41ec79b56010dce4e9c3eb1a4d6ee1122
kartik-malviya1 <kartikmalviya30@gmail.com>
2024-06-20 20:40:25 +0530

    Create files in Utils

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env"
 })
 
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000)
+  console.log(`Server is running at Port: ${process.env.PORT}`);
+  app.on("Error ",(err)=>{
+    console.log("Error: ", err);
+    throw err
+  }) 
+})
+.catch((err)=>{
+  console.log("MongoDB connection failde !!", err);
+})
 
 
 

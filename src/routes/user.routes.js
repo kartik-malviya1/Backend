793f00d7caebb3b0ca85c6377bc708fd380ae38b
kartik-malviya1 <kartikmalviya30@gmail.com>
2024-06-26 00:42:57 +0530

    add login and logout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express'
-import { registerUser } from '../controllers/user.controller.js'
+import { loginUser, logoutUser, registerUser } from '../controllers/user.controller.js'
 import { upload } from '../middlewares/multer.middleware.js'
+import { verifyJWT } from '../middlewares/auth.middleware.js'
+
 
 const router = Router()
 
@@ -18,4 +20,8 @@ router.route("/register").post(
   registerUser
 )
 
-export default router
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT, logoutUser)
+
+export default router 
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,7 +1,7 @@
 class ApiResponse {
   constructor(data, statusCode, message = "Success") {
-    this.data = data
     this.statusCode = statusCode
+    this.data = data
     this.message = message
     this.success = statusCode < 400
   }
